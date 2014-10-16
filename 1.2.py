94cde6b894aea23125d3daa05a49299e68d3a85c
Marc Gonzalez <40756@longroad.ac.uk>
2014-10-16 12:08:10 +0100

    1.2

---
+++ 1.2.py
@@ -0,0 +1,10 @@
+# Marc Gonzalez
+#16/10/14
+#1.2
+
+message = input( "please enter message: ")
+number = int(input("please enter number of repetions you want: "))
+
+for count in range(number):
+    print(message)
+    
