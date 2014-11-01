0b6454b394ba044676bd4cf2305d766bf169fdc1
Marc Gonzalez <40756@student.longroad.ac.uk>
2014-11-01 15:58:10 +0000

    iteration 1.4

---
+++ 1.4.py
@@ -0,0 +1,10 @@
+#Marc Gonzalez
+#01/11/14
+#iteration 1.4
+
+number = int(input("please enetr a number between 10 and 20 inclusive: "))
+
+while not 9<number<21:
+    number = int(input("please eneter a better number: "))
+
+print("thanks")
