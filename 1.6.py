2c906b2e5602492329ef5b3fcdc2185222b2a6b6
Marc Gonzalez <40756@student.longroad.ac.uk>
2014-11-03 18:45:58 +0000

    iteration 1.6

---
+++ 1.6.py
@@ -0,0 +1,10 @@
+#Marc Gonzalez
+#03/11/14
+#iteration 1.6
+
+for count in range(1,21):
+    pounds_1 = count * 2.2
+    pounds = round(pounds_1, 1)
+    print("  {0} kg {1} lb".format(count , pounds))
+    
+
